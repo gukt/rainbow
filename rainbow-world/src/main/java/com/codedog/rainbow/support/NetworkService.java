addfa00708f9eb271119ff517b8b37250d11df2c
gukt <29283212@qq.com>
2021-07-15 21:54:05 +0800

    Refine comment

--- rainbow-world/src/main/java/com/codedog/rainbow/support/NetworkService.java
+++ rainbow-world/src/main/java/com/codedog/rainbow/support/NetworkService.java
@@ -12,7 +12,7 @@ public interface NetworkService extends Lifecycle {
     /**
      * 获得网络服务的真实端口号（当使用随机端口的时候需要）
      *
-     * @return 返回随机的端口号
+     * @return 真实的端口号
      * @throws IllegalStateException if service is not running
      */
     int getRealPort();
