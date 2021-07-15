870075052a52b242dac857f35e850d6ebe540729
gukt <29283212@qq.com>
2021-07-15 18:37:55 +0800

    add rainbow-world sub module

---
+++ rainbow-world/src/main/java/com/codedog/rainbow/RainbowWorldApplication.java
@@ -0,0 +1,13 @@
+package com.codedog.rainbow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RainbowWorldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RainbowWorldApplication.class, args);
+    }
+
+}
