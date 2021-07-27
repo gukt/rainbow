e97d37d88111d8593e2aa5d1c16e7a8b2af8e0a5
gukt <gukaitong@gmail.com>
2021-07-27 23:38:18 +0800

    Refine Comments

--- rainbow-core/src/main/java/com/codedog/rainbow/core/AbstractLifecycle.java
+++ rainbow-core/src/main/java/com/codedog/rainbow/core/AbstractLifecycle.java
@@ -7,30 +7,49 @@ package com.codedog.rainbow.core;
 import lombok.Getter;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
 /**
- * AbstractLifeCycleAware
- * TODO 换成列表，可添加多个listener
+ * AbstractLifecycle
  *
  * @author https://github.com/gukt
  */
 public abstract class AbstractLifecycle implements Lifecycle {
 
     private final List<Listener> listeners = new ArrayList<>();
-    @Getter
-    protected long startTime;
-    @Getter
-    protected volatile Lifecycle.State state = State.NEW;
-    @Getter
-    protected LifecycleException failureCause;
+    /**
+     * 启动时间，单位：毫秒
+     */
+    @Getter protected long startTime;
+    /**
+     * 状态
+     */
+    @Getter protected volatile State state = State.NEW;
+    /**
+     * 失败原因
+     */
+    @Getter protected LifecycleException failureCause;
 
     @Override
     public Lifecycle addListener(Listener listener) {
+        Objects.requireNonNull(listener, "listener is null (expected: not null)");
         listeners.add(listener);
         return this;
     }
 
+    @Override
+    public Lifecycle addListener(Listener... listeners) {
+        Collections.addAll(this.listeners, listeners);
+        return this;
+    }
+
+    /**
+     * 设置状态。当状态成功变更后，所有注册的 {@link Listener listeners} 会被逐一调用。
+     *
+     * @param state new {@link State State}
+     */
     protected void setState(State state) {
         // TODO 加状态变更检查，不是所有状态之间都可以互转
         State from = this.state;
@@ -51,6 +70,21 @@ public abstract class AbstractLifecycle implements Lifecycle {
         });
     }
 
+    /**
+     * 创建并抛出 {@link LifecycleException } 异常。
+     *
+     * @param message 错误消息
+     */
+    protected void fail(String message) {
+        fail(message, null);
+    }
+
+    /**
+     * 创建并抛出 {@link LifecycleException } 异常。
+     *
+     * @param message 错误消息
+     * @param cause   引起异常的 {@link Throwable 原因}
+     */
     protected void fail(String message, Throwable cause) {
         failureCause = new LifecycleException(message, cause);
         setState(State.FAILED);
--- rainbow-core/src/main/java/com/codedog/rainbow/core/Lifecycle.java
+++ rainbow-core/src/main/java/com/codedog/rainbow/core/Lifecycle.java
@@ -14,8 +14,8 @@ public interface Lifecycle {
     /**
      * 启动服务
      *
-     * @throws IllegalStateException if already started
-     * @throws LifecycleException    if interrupted or unable to bind
+     * @throws IllegalStateException 如果服务当前已经启动或正则启动中
+     * @throws LifecycleException    如果被其他线程中断；或绑定端口失败（对于 TCP 服务来说）
      */
     void start();
 
@@ -27,35 +27,35 @@ public interface Lifecycle {
     void stop();
 
     /**
-     * 获取服务的启动时间
+     * 获取服务的启动时间，单位：毫秒。
      */
     long getStartTime();
 
     /**
-     * 当前是否处于 RUNNING 状态
+     * 当前是否处于 {@link State#RUNNING RUNNING} 状态。
      *
-     * @return true if running，otherwise false.
+     * @return 如果是，返回 <code>true</code>; 反之 <code>false</code>
      */
     default boolean isRunning() {
         return getState() == State.RUNNING;
     }
 
     /**
-     * 返回生命周期状态
+     * 返回生命周期 {@link State 状态}。
      */
     State getState();
 
     /**
-     * 引起失败的原因
+     * 返回引起失败的 {@link Throwable 原因}。
      *
-     * @return 返回引起失败的原因
+     * @return 返回引起失败的 {@link Throwable 原因}
      */
     Throwable getFailureCause();
 
     /**
-     * 检查服务的当前状态是不是 {@link State#NEW}
+     * 检查服务的当前状态是不是 {@link State#NEW NEW}。
      *
-     * @throws IllegalStateException if {@link #getState()} is not {@link State#NEW}
+     * @throws IllegalStateException if {@link #getState()} is not {@link State#NEW NEW}
      */
     default void requireStateNew() {
         if (getState() != State.NEW) {
@@ -64,9 +64,9 @@ public interface Lifecycle {
     }
 
     /**
-     * 检查服务的当前状态是不是 {@link State#RUNNING}
+     * 检查服务的当前状态是不是 {@link State#RUNNING RUNNING}。
      *
-     * @throws IllegalStateException if {@link #getState()} is not {@link State#RUNNING}
+     * @throws IllegalStateException if {@link #getState()} is not {@link State#RUNNING RUNNING}
      */
     default void requireStateRunning() {
         if (getState() != State.RUNNING) {
@@ -75,14 +75,28 @@ public interface Lifecycle {
     }
 
     /**
-     * 添加生命周期监听器，可以连续添加多个
+     * 添加 {@link Listener 监听器}。
      *
-     * @param listener 监听器
-     * @return {@linkplain Lifecycle this}
+     * @param listener {@link Listener 监听器}
+     * @return 当前对象自身
+     * @throws NullPointerException 如果参数 listener 为 null
      */
     Lifecycle addListener(Listener listener);
 
+    /**
+     * 添加多个 {@link Listener 监听器}。
+     *
+     * @param listeners {@link Listener 监听器} 数组
+     * @return 当前对象自身
+     * @throws NullPointerException 如果参数 listener 为 null
+     */
+    Lifecycle addListener(Listener... listeners);
+
+    /**
+     * 用来表示状态的通用枚举。
+     */
     enum State {
+
         /**
          * 未启动
          */
@@ -114,44 +128,41 @@ public interface Lifecycle {
         FAILED
     }
 
+    /**
+     * {@link Lifecycle 生命周期对象} 的 {@link State 状态} 变更监听器。
+     */
     class Listener {
 
         /**
-         * 当状态从 NEW 变成 STARTING 时调用
+         * 当状态从 {@link State#NEW NEW} 变成 {@link State#STARTING STARTING} 时调用。
          */
-        public void starting() {
-        }
+        public void starting() { }
 
         /**
-         * 当状态从 STARTING 变成 RUNNING 时调用
+         * 当状态从 {@link State#STARTING STARTING} 变成 {@link State#RUNNING RUNNING} 时调用。
          */
-        public void running() {
-        }
+        public void running() { }
 
         /**
-         * 当状态变成 STOPPING 时调用，变化之前的状态可能是 STARTING、RUNNING
-         * from 参数表示变化之前的状态
+         * 当状态变成 {@link State#STOPPING STOPPING} 时调用，变化之前的状态可能是 STARTING、RUNNING
          *
          * @param from 变化之前的状态
          */
-        public void stopping(State from) {
-        }
+        public void stopping(State from) { }
 
         /**
-         * 当状态变成 TERMINATED 时调用，TERMINATED 是最终状态
+         * 当状态变成 {@link State#TERMINATED TERMINATED} 时调用。TERMINATED 是最终状态。
          *
          * @param from 变化之前的状态
          */
-        public void terminated(State from) {
-        }
+        public void terminated(State from) { }
 
         /**
-         * 当状态变化为 FAILURE 时调用，
+         * 当发生异常时调用。
          *
          * @param from    变化之前的状态
-         * @param failure 导致失败的异常
+         * @param failure 导致失败的原因
          */
-        public void failed(State from, Throwable failure) {
-        }
+        public void failed(State from, Throwable failure) { }
     }
 }
