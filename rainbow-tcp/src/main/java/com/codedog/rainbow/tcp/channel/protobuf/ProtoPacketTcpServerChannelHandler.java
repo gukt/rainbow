5ba1f89cd7c39f173b4cf0bf5464456603d70485
gukt <gukaitong@gmail.com>
2021-08-05 16:19:26 +0800

    Update beforeWrite logic

--- rainbow-tcp/src/main/java/com/codedog/rainbow/tcp/channel/protobuf/ProtoPacketTcpServerChannelHandler.java
+++ rainbow-tcp/src/main/java/com/codedog/rainbow/tcp/channel/protobuf/ProtoPacketTcpServerChannelHandler.java
@@ -4,15 +4,14 @@
 
 package com.codedog.rainbow.tcp.channel.protobuf;
 
-import com.codedog.rainbow.lang.TypeMismatchException;
 import com.codedog.rainbow.tcp.TcpProperties;
 import com.codedog.rainbow.tcp.channel.TcpServerChannelHandler;
 import com.codedog.rainbow.tcp.message.MessageHandler;
 import com.codedog.rainbow.tcp.message.MessageHandler.Error;
-import com.codedog.rainbow.tcp.message.MessageHandlerException;
 import com.codedog.rainbow.tcp.session.DefaultSession;
 import com.codedog.rainbow.tcp.session.Session;
 import com.codedog.rainbow.tcp.util.ProtoUtils;
+import com.codedog.rainbow.util.Assert;
 import com.codedog.rainbow.world.generated.CommonProto.ProtoPacket;
 import com.codedog.rainbow.world.generated.CommonProto.ProtoPacketOrBuilder;
 import com.google.protobuf.MessageLiteOrBuilder;
@@ -38,17 +37,20 @@ public final class ProtoPacketTcpServerChannelHandler extends TcpServerChannelHa
             @Override
             public Object beforeWrite(Object message) {
                 message = super.beforeWrite(message);
-                if (!isTypeSupported(message)) {
-                    throw new TypeMismatchException(message, MessageHandler.Error.class, MessageHandlerException.class);
-                }
-                // 将 “错误或异常” 转换一下
+                // 只能发送 Error 或 MessageLiteOrBuilder 类型
+                Assert.isAssignableFromAny(message.getClass(), MessageHandler.Error.class, MessageLiteOrBuilder.class);
+                // 处理 Error
                 if (message instanceof MessageHandler.Error) {
                     message = ProtoUtils.errorOf((Error) message);
                 }
-                MessageLiteOrBuilder msg = ProtoUtils.requireMessageLiteOrBuilder(message, "message");
+                // 处理 MessageLiteOrBuilder
+                MessageLiteOrBuilder msg = (MessageLiteOrBuilder) message;
+                // 如果返回的只是 Payload 数据（不是 ProtoPacketOrBuilder）则需要 wrap 一下
                 if (!(msg instanceof ProtoPacketOrBuilder)) {
                     msg = ProtoUtils.wrapPacket(msg);
                 }
+                // 至此，msg 一定是 ProtoPacketOrBuilder 类型了。但此时仍不能确定它是不是 Builder 对象。
+                // 因为后面需要 Builder 对象进行赋值，所以需要先获得 Builder 对象。
                 ProtoPacket.Builder builder = ProtoUtils.safeGetBuilder(msg);
                 int sn = store.incrSequenceNumberAndGet();
                 builder.setSn(sn);
